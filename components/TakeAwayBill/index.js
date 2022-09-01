8ce0cc8b12b07ad2105dc4724ece271016da9aa4
Abdelhamid yehia <abdd.yahia@gmail.com>
2022-09-01 13:57:53 +0200

    oncreation-1/9-drag&drop

--- components/TakeAwayBill/index.js
+++ components/TakeAwayBill/index.js
@@ -1,4 +1,4 @@
-import {ErrorMessage, Field, Form, Formik} from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
 import BillOrderCard from "../BillOrderCard";
 import styles from "./index.module.scss";
@@ -6,180 +6,160 @@ import * as yup from "yup";
 import Image from "next/image";
 import divider from "../../public/images/divider.svg";
 import BillOptions from "../BillOptions";
+import { useDrop } from "react-dnd/dist/hooks/useDrop/useDrop";
 const TakeAwayBill = () => {
-	let schema = yup.object().shape({
-		name: yup
-			.string()
-			.required("Required")
-			.min(2, "Too Short!")
-			.max(60, "Too Long!"),
-		phone: yup
-			.string()
-			.required("Required")
-			.min(11, "Too Short!")
-			.max(11, "Too Long!")
-	});
-	return (
-		<div
-			className={`${styles.BillContainer} h-100 d-flex flex-column `}
-		>
-			<div className="row ">
-				<div className={`col-8 ${styles.orderNumber}`}>Order Bill # 1</div>
-				<div className={`col-4 ${styles.orderType} text-end`}>
-					{" "}
-					Take Away
-				</div>
-			</div>
-			<div className={`${styles.orderDate} `}>
-				January 23, 2022 - 5:00 pm
-			</div>
-			<div className={`pt-1 d-flex flex-column overflow-auto pe-1`}>
-				<BillOrderCard
-					title="Fettuccine Chicken Alferdo"
-					price="68.34"
-					size={"M"}
-					quantity="1"
-				/>
-				<BillOrderCard
-					title="Spicy - Fettuccine Chicken"
-					price="68.34"
-					size={"S"}
-					quantity="1"
-				/>
-				<BillOrderCard
-					title="Fettuccine Chicken Alferdo"
-					price="68.34"
-					size={"M"}
-					quantity="1"
-				/>
-			</div>
-			<div className={`mt-auto ${styles.optionsContainer}`}>
-				<div
-					className={`accordion`}
-					valign="bottom"
-					id="accordionPanelsStayOpenExample"
-				>
-					<div className="accordion-item mt-2">
-						<h2
-							className="accordion-header"
-							id="panelsStayOpen-headingOne"
-						>
-							<button
-								className="accordion-button"
-								type="button"
-								data-bs-toggle="collapse"
-								data-bs-target="#panelsStayOpen-collapseOne"
-								aria-expanded="true"
-								aria-controls="panelsStayOpen-collapseOne"
-							>
-								Client information
-							</button>
-						</h2>
-						<div
-							id="panelsStayOpen-collapseOne"
-							className="accordion-collapse collapse show "
-							aria-labelledby="panelsStayOpen-headingOne"
-						>
-							<div className="accordion-body p-0 pt-2">
-								<Formik
-									initialValues={{
-										name: "",
-										phone: ""
-									}}
-									validationSchema={schema}
-									onSubmit={(values) => {
-										console.log(values);
-									}}
-								>
-									{({touched, errors, handleSubmit}) => (
-										<Form className={`row g-3`}>
-											<div
-												className={`col-md-12 ${styles.inputText}`}
-											>
-												<label
-													htmlFor="inputname"
-													className="form-label mb-0"
-												>
-													client name
-												</label>
-												<Field
-													type="text"
-													name="name"
-													className={`${
-														styles.inputField
-													} form-control ${
-														touched.name && errors.name
-															? "is-invalid"
-															: ""
-													}`}
-													id="inputname"
-												/>
-												<ErrorMessage
-													component="div"
-													name="name"
-													className="invalid-feedback"
-												/>
-											</div>
-											<div
-												className={`col-md-12 ${styles.inputText} mt-2`}
-											>
-												<label
-													htmlFor="inputphone"
-													className="form-label mb-0"
-												>
-													client phone
-												</label>
-												<Field
-													type="phone"
-													name="phone"
-													className={`${
-														styles.inputField
-													}  form-control ${
-														touched.phone && errors.phone
-															? "is-invalid"
-															: ""
-													}`}
-													id="inputphone"
-												/>
-												<ErrorMessage
-													component="div"
-													name="phone"
-													className="invalid-feedback"
-												/>{" "}
-											</div>
-										</Form>
-									)}
-								</Formik>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="py-2">
-					<Image src={divider} layout="responsive" alt="divider" />
-				</div>
-				<div className="pt-1 row">
-					<div className={`col-8 ${styles.subPriceText}`}>Subtotal</div>
-					<div className={`col-4 ${styles.subPriceNumber} text-end`}>
-						72 EGP
-					</div>
-					<div className={`col-8 ${styles.subPriceText}`}>taxes</div>
-					<div className={`col-4 ${styles.subPriceNumber} text-end`}>
-						12 EGP
-					</div>
-					<div className="py-2">
-						<Image src={divider} alt="divider" layout="responsive" />
-					</div>
-					<div className={`col-8 ${styles.totalText}`}>Total</div>
-					<div className={`col-4 ${styles.totalNumber} text-end`}>
-						84 EGP
-					</div>
-					<div className="pt-1 mb-1 col-12">
-						<BillOptions />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "div",
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+    }),
+    drop: (item) => {
+      console.log(item);
+    },
+  }));
+  let schema = yup.object().shape({
+    name: yup
+      .string()
+      .required("Required")
+      .min(2, "Too Short!")
+      .max(60, "Too Long!"),
+    phone: yup
+      .string()
+      .required("Required")
+      .min(11, "Too Short!")
+      .max(11, "Too Long!"),
+  });
+  return (
+    <div className={`${styles.BillContainer} h-100 d-flex flex-column `}>
+      <div className="row ">
+        <div className={`col-8 ${styles.orderNumber}`}>Order Bill # 1</div>
+        <div className={`col-4 ${styles.orderType} text-end`}> Take Away</div>
+      </div>
+      <div className={`${styles.orderDate} `}>January 23, 2022 - 5:00 pm</div>
+      <div ref={drop} className={`pt-1 d-flex flex-column overflow-auto pe-1`}>
+        <BillOrderCard
+          title="Fettuccine Chicken Alferdo"
+          price="68.34"
+          size={"M"}
+          quantity="1"
+        />
+        <BillOrderCard
+          title="Spicy - Fettuccine Chicken"
+          price="68.34"
+          size={"S"}
+          quantity="1"
+        />
+        <BillOrderCard
+          title="Fettuccine Chicken Alferdo"
+          price="68.34"
+          size={"M"}
+          quantity="1"
+        />
+      </div>
+      <div className={`mt-auto ${styles.optionsContainer}`}>
+        <div
+          className={`accordion`}
+          valign="bottom"
+          id="accordionPanelsStayOpenExample"
+        >
+          <div className="accordion-item mt-2">
+            <h2 className="accordion-header" id="panelsStayOpen-headingOne">
+              <button
+                className="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseOne"
+                aria-expanded="true"
+                aria-controls="panelsStayOpen-collapseOne"
+              >
+                Client information
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseOne"
+              className="accordion-collapse collapse show "
+              aria-labelledby="panelsStayOpen-headingOne"
+            >
+              <div className="accordion-body p-0 pt-2">
+                <Formik
+                  initialValues={{
+                    name: "",
+                    phone: "",
+                  }}
+                  validationSchema={schema}
+                  onSubmit={(values) => {
+                    console.log(values);
+                  }}
+                >
+                  {({ touched, errors, handleSubmit }) => (
+                    <Form className={`row g-3`}>
+                      <div className={`col-md-12 ${styles.inputText}`}>
+                        <label htmlFor="inputname" className="form-label mb-0">
+                          client name
+                        </label>
+                        <Field
+                          type="text"
+                          name="name"
+                          className={`${styles.inputField} form-control ${
+                            touched.name && errors.name ? "is-invalid" : ""
+                          }`}
+                          id="inputname"
+                        />
+                        <ErrorMessage
+                          component="div"
+                          name="name"
+                          className="invalid-feedback"
+                        />
+                      </div>
+                      <div className={`col-md-12 ${styles.inputText} mt-2`}>
+                        <label htmlFor="inputphone" className="form-label mb-0">
+                          client phone
+                        </label>
+                        <Field
+                          type="phone"
+                          name="phone"
+                          className={`${styles.inputField}  form-control ${
+                            touched.phone && errors.phone ? "is-invalid" : ""
+                          }`}
+                          id="inputphone"
+                        />
+                        <ErrorMessage
+                          component="div"
+                          name="phone"
+                          className="invalid-feedback"
+                        />{" "}
+                      </div>
+                    </Form>
+                  )}
+                </Formik>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="py-2">
+          <Image src={divider} layout="responsive" alt="divider" />
+        </div>
+        <div className="pt-1 row">
+          <div className={`col-8 ${styles.subPriceText}`}>Subtotal</div>
+          <div className={`col-4 ${styles.subPriceNumber} text-end`}>
+            72 EGP
+          </div>
+          <div className={`col-8 ${styles.subPriceText}`}>taxes</div>
+          <div className={`col-4 ${styles.subPriceNumber} text-end`}>
+            12 EGP
+          </div>
+          <div className="py-2">
+            <Image src={divider} alt="divider" layout="responsive" />
+          </div>
+          <div className={`col-8 ${styles.totalText}`}>Total</div>
+          <div className={`col-4 ${styles.totalNumber} text-end`}>84 EGP</div>
+          <div className="pt-1 mb-1 col-12">
+            <BillOptions />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TakeAwayBill;
--- pages/take-away/index.js
+++ pages/take-away/index.js
@@ -1,42 +1,52 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import FoodItem from "../../components/FoodItem";
 import FoodType from "../../components/FoodType";
 import ItemsList from "../../components/ItemsList";
 import OrderBillMain from "../../components/OrderBillMain";
 import OrderType from "../../components/OrderType";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import styles from "./index.module.scss";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
+import { DragDropContext } from "react-beautiful-dnd";
+
 const TakeAway = () => {
-	const [orderType, setOrderType] = useState("Dine Inn");
-	const [foodType, setfoodType] = useState("Pizza");
-	const printState = useSelector((state) => state.printState.data);
-	const variants = {
-		hidden: {opacity: 0, y: 200},
-		enter: {opacity: 1, y: 0},
-		exit: {opacity: 0, y: 0}
-	};
-	return (
-		<>
-			<motion.div
-				className={`row mx-0 px-0 pt-4 pb-0 mb-0 h-100 ${
-					printState == true && styles.translat
-				}`}
-			>
-				<div className={`col-xxl-9 col-lg-8 col-md-6 col-12 h-100`}>
-					<OrderType orderType={orderType} setOrderType={setOrderType} />
-					<FoodType
-						orderType={orderType}
-						foodType={foodType}
-						setfoodType={setfoodType}
-					/>
-					<ItemsList orderType={orderType} foodType={foodType} />
-				</div>
-				<div className={`col-xxl-3 col-lg-4 col-md-6 col-12 h-100`}>
-					<OrderBillMain orderType={orderType} />
-				</div>
-			</motion.div>
-			{/* <div
+  const [orderType, setOrderType] = useState("Dine Inn");
+  const [foodType, setfoodType] = useState("Pizza");
+  const printState = useSelector((state) => state.printState.data);
+  const variants = {
+    hidden: { opacity: 0, y: 200 },
+    enter: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: 0 },
+  };
+  return (
+    <>
+      <DndProvider
+        backend={HTML5Backend}
+        options={{ enableTouchEvents: false, enableMouseEvents: true }}
+      >
+        <motion.div
+          className={`row mx-0 px-0 pt-4 pb-0 mb-0 h-100 ${
+            printState == true && styles.translat
+          }`}
+        >
+          <div className={`col-xxl-9 col-lg-8 col-md-6 col-12 h-100`}>
+            <OrderType orderType={orderType} setOrderType={setOrderType} />
+            <FoodType
+              orderType={orderType}
+              foodType={foodType}
+              setfoodType={setfoodType}
+            />
+            <ItemsList orderType={orderType} foodType={foodType} />
+          </div>
+          <div className={`col-xxl-3 col-lg-4 col-md-6 col-12 h-100`}>
+            <OrderBillMain orderType={orderType} />
+          </div>
+        </motion.div>
+      </DndProvider>
+      {/* <div
 				className={`row position-absolute top-0 ${
 					printState == true
 						? styles.showPrint
@@ -52,8 +62,8 @@ const TakeAway = () => {
 					<OrderBillMain orderType={orderType} />
 				</div>
 			</div> */}
-		</>
-	);
+    </>
+  );
 };
 
 export default TakeAway;

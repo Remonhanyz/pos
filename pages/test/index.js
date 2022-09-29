591162b76f24e3e86e86e5e0115e1f2a162e0ee4
Remonhanyz <34352561+Remonhanyz@users.noreply.github.com>
2022-09-29 12:18:46 +0200

    3kkkkt

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,40 +1,83 @@
-import TextField from "@mui/material/TextField";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { Form, Formik, useFormik } from "formik";
-import * as React from "react";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import { Box, Container, Grid, Paper, TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import { Field, Form, Formik } from "formik";
+import { DesktopDatePicker } from "formik-mui-lab";
+import React from "react";
 import * as yup from "yup";
 
+const DisplayingErrorMessagesSchema = yup.object().shape({
+  startDate: yup
+    .date()
+    .nullable()
+    .typeError("Start date is required")
+    .required("Start Date is required"),
 
-export default function Testing() {
-  const [value, setValue] = React.useState(null);
-  const validationSchema = yup.object({
-    date: yup
-      .string("Enter date")
-      .required("date is required"),
-  });
-  const formik = useFormik({
-    initialValues: {
-      date: new Date("2015-03-25"),
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+  endDate: yup
+    .date()
+    .nullable()
+    .when(
+      "startDate",
+      (startDate, yup) =>
+        startDate && yup.min(startDate, "End date cannot be before start time")
+    )
+    .required("End Date is required")
+    .typeError("Enter a value End date"),
+});
+
+export default function DateValidationWithFormik() {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <form className="w-100 m-4 px-4" onSubmit={formik.handleSubmit}>
-        <DatePicker
-          label="date"
-          renderInput={(params) => <TextField {...params} />}
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-      </form>
-    </LocalizationProvider>
+    <>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Formik
+          initialValues={{
+            startDate: "",
+            endDate: "",
+          }}
+          validationSchema={DisplayingErrorMessagesSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              setSubmitting(false);
+              alert(JSON.stringify(values, null, 2));
+            }, 500);
+          }}
+        >
+          {({ errors, touched, values }) => (
+            <Form>
+              <Grid container spacing={2}>
+                <Grid item xs={12} align="center">
+                  <h1>Formik Validate Date Pickers</h1>
+                </Grid>
+                <Grid item xs={3} align="left"></Grid>
+                <Grid item xs={3} align="right">
+                  <Field
+                    component={DesktopDatePicker}
+                    disablePast
+                    views={["year", "month", "day"]}
+                    name="startDate"
+                    label="Start Date"
+                  />
+                </Grid>
+                <Grid item xs={3} align="left">
+                  <Field
+                    component={DesktopDatePicker}
+                    name="endDate"
+                    label="End Date"
+                    views={["year", "month", "day"]}
+                  />
+                </Grid>
+                <Grid item xs={3} align="left"></Grid>
+                <Grid item xs={12} align="center">
+                  <Button type="submit" variant="contained" color="primary">
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      </LocalizationProvider>
+    </>
   );
 }
